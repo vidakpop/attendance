077ad5fcfca35d75b3085261aced99c0ae9958e6
vidakpop <daviskpop@gmail.com>
2025-05-28 16:17:10 +0300

    add class user

--- attendance/models.py
+++ attendance/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser)
+class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
+
+
     
