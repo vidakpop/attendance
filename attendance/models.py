c18767419c23767040a0fdcc56d1fea0b0f1b6b5
vidakpop <daviskpop@gmail.com>
2025-05-28 16:21:54 +0300

    add class model

--- attendance/models.py
+++ attendance/models.py
@@ -4,5 +4,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
 
-
+class Class(models.Model):
+    name = models.Charfield(max_length=100)
+    teacher = models.Foreinkey(User, on_delete=models.CASCADE,related_name='classes')
+    
+    def _str__(self):
+        return self.name
     
