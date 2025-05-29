47c2dfa066a569ae6b50a3a3205ee1bedd18b14c
vidakpop <daviskpop@gmail.com>
2025-05-29 19:58:01 +0300

    Add initial viewset and serializer imports to views.py

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets,permissions ,status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Class,Student,Attendance
+from .serializer import ClassSerializer,StudentSerializer,AttendanceSerializer
+from django.utils import timezone
 
-# Create your views here.
