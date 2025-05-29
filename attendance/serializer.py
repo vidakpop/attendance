ef3ec0c51dc6703ceb91bdf515d018581ca36047
vidakpop <daviskpop@gmail.com>
2025-05-29 19:49:46 +0300

    Fix ClassSerializer model reference and ensure consistent formatting in serializers

--- attendance/serializer.py
+++ attendance/serializer.py
@@ -5,3 +5,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','username','is_teacher']
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Class
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Student
+        fields = '__all__'
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    Student = StudentSerializer(read_only=True)
+
+    class Meta:
+        model=Attendance
+        fields = '__all__'
+        
