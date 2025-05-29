268b2776630b48e3cdea1452378085cbd638e2f5
vidakpop <daviskpop@gmail.com>
2025-05-29 19:32:44 +0300

    Add AUTH_USER_MODEL setting to specify custom user model

--- backend/settings.py
+++ backend/settings.py
@@ -80,9 +80,11 @@ CORS_ALLOW_ALL_ORIGINS =True
 REST_FRAMEWORK ={
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        
+
     )
 }
+
+AUTH_USER_MODEL = 'attendance.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
