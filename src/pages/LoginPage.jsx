197779dda3bcbc473791bdaf442a58b3d7dfcd28
vidakpop <daviskpop@gmail.com>
2025-05-30 13:24:39 +0300

    feat: add login form structure to LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -26,6 +26,11 @@ const LoginPage = () => {
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100'>
       <form onSubmit={login} className='bg-white p-8 rounded shadow-md w-96'>
+        <h2 className='text-2xl font-bold mb-4'>Teacher Login</h2>
+         <input className='w-full mb-2 p-2 border' placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}/>
+         <input type='password' className='w-full mb-4 p-2 border' placeholder='Password'/>
+         <button className='w-full  bg-blue-500 text-white p4 rounded' type='submit'>Login</button>
+
 
       </form>
 
