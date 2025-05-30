fc320c67acb509470c3fd81d3cc5718ae2a706c9
vidakpop <daviskpop@gmail.com>
2025-05-30 10:14:42 +0300

    Register Attendance, Student, Class, and User models in the admin site

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Attendance, Student, Class, User
+admin.site.register(User)
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Attendance)
 
-# Register your models here.
+
