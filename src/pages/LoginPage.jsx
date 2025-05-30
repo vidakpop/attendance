3c3408ea0f9cb78bec72066447f89a768f1736b7
vidakpop <daviskpop@gmail.com>
2025-05-30 13:00:10 +0300

    feat: implement login functionality in LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,24 @@ import axios from 'axios'
 const LoginPage = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const navigate =useNavigate()
+    const navigate = useNavigate()
+
+    const login = async (e) => {
+      e.preventDefault()
+      try {
+        const res = await axios.post('http://localhost:8000/api/token/',{
+          username,
+          password
+        })
+        localStorage.setItem('access', res.data.access)
+        localStorage.setItem('refresh', res.data.refresh)
+        navigate('/dashboard')
+      }
+      catch (error) {
+        alert('Login failed. Please check your credentials.')
+      }
+    }
+
   return (
     <div>LoginPage</div>
   )
