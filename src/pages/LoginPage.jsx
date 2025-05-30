4b5d76f73170efc174ae0e90fc3c5010b85fc7e6
vidakpop <daviskpop@gmail.com>
2025-05-30 12:35:34 +0300

    feat: add state management and navigation to LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 const LoginPage = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate =useNavigate()
   return (
     <div>LoginPage</div>
   )
