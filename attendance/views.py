d68e63507cd186654155d7f02ef22eb1d287ca4a
vidakpop <daviskpop@gmail.com>
2025-06-18 17:44:58 +0300

    Enhance StudentViewSet get_queryset method to filter students by class_id or school_class for teachers

--- attendance/views.py
+++ attendance/views.py
@@ -29,14 +29,22 @@ class StudentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-
+        # Check for either parameter name (class_id or school_class)
+        class_id = self.request.query_params.get('class_id', None)
+        if class_id is None:
+            class_id = self.request.query_params.get('school_class', None)
+            
+        print(f"Filtering students with class_id: {class_id}")
+        
         if user.is_teacher:
-            teacher_classes = Class.objects.filter(teacher=user)
-            return Student.objects.filter(school_class__in=teacher_classes).distinct()
+            if class_id:
+                # If class_id is provided, ONLY return students from that specific class
+                return Student.objects.filter(school_class_id=class_id, school_class__teacher=user).distinct()
+            else:
+                # If no class_id, return all students taught by this teacher
+                return Student.objects.filter(school_class__teacher=user).distinct()
         else:
             return Student.objects.none()
- 
-        
 
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
